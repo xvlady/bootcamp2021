9328db473a1ede6d29cfaa2a17dc8ee79e6d4fbc
xvv <xw@ya.ru>
2021-04-25 19:45:47 +0500

    add index.html

---
+++ adminer/index.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+if (basename($_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
+    header('Content-Type: text/css');
+    readfile(__DIR__ . '/adminer.css');
+    exit;
+}
+
+function adminer_object(): Adminer
+{
+    return new class extends Adminer
+    {
+        public function name(): ?string
+        {
+            return $this->getEnv('ADMINER_NAME') ?? parent::name();
+        }
+
+        public function loginForm(): void
+        {
+            parent::loginForm();
+
+            if ($this->getEnv('ADMINER_AUTOLOGIN')) {
+                echo script('
+                    document.addEventListener(\'DOMContentLoaded\', function () {
+                        document.forms[0].submit()
+                    })
+                ');
+            }
+        }
+
+        public function loginFormField($name, $heading, $value): string
+        {
+            $envValue = $this->getLoginConfigValue($name);
+
+            if ($envValue !== null) {
+                $value = sprintf(
+                    '<input name="auth[%s]" type="%s" value="%s">',
+                    h($name),
+                    h($name === 'password' ? 'password' : 'text'),
+                    h($envValue)
+                );
+            }
+
+            return parent::loginFormField($name, $heading, $value);
+        }
+
+        public function getLoginConfigValue(string $key): ?string
+        {
+            switch ($key) {
+                case 'db': return $this->getEnv('ADMINER_DEFAULT_DB_NAME');
+                case 'driver': return $this->getEnv('ADMINER_DEFAULT_DB_DRIVER');
+                case 'password': return $this->getEnv('ADMINER_PASSWORD');
+                case 'server': return $this->getEnv('ADMINER_DEFAULT_SERVER');
+                case 'username': return $this->getEnv('ADMINER_USERNAME');
+                case 'name': return $this->getEnv('ADMINER_NAME');
+                default: return null;
+            }
+        }
+
+        private function getEnv(string $key): ?string
+        {
+            return getenv($key) ?: null;
+        }
+    };
+}
+
+require __DIR__ . '/adminer.php';
